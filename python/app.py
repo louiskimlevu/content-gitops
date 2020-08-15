a008af7e45dc55c7b5eb8580e2c1a70941417a0b
Louis Kim Le Vu <57093546+louiskimlevu@users.noreply.github.com>
2020-08-15 17:30:45 +0800

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "This is a new version v5"
+    return "This is a new version v6, this is with gitflow"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
